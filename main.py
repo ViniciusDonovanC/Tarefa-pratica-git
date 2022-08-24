1b95f73af0c70c298220a4d670ec65298e013cf0
Luis0756 <105085312+Luis0756@users.noreply.github.com>
2022-08-24 19:48:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-# coding=utf-8
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def fatorial(num):
+    i = num
+    while i > 1:
 
+        i -= 1
+        num *= i
 
-# Press the green button in the gutter to run the script.
+    return num
 
+x = input()
 
+print 'fatorial: ', fatorial(x)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
