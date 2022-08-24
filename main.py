8b0991ac4c5f67e354b0dea8debf3bb445d4b69e
DanielUnifil <105524050+DanielUnifil@users.noreply.github.com>
2022-08-24 20:11:32 -0300

    Calculadora de Média

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-# Calculo IMC
-def imc():
-    p = float(input('Informe seu Peso(Kg):'))
-    h = float(input('Informe sua Altura(m):'))
+# Calculadora de Média utilizando pesos diferentes para cada nota
 
-    x = p / (h*h)
+nota_1 = int(input('Insira a primeira nota: '));
+pesoNota_1 = int(input('Insira o peso da nota 1: '))
+nota_2 = int(input('Insira a segunda nota: '));
+pesoNota_2 = int(input('Insira o peso da nota 1: '))
 
-    return print(f'Seu IMC é {x}')
+media = float;
+nota_1 *= pesoNota_1;
+nota_2 *= pesoNota_2;
 
+media = (nota_1 + nota_2) / (pesoNota_1 + pesoNota_2);
 
-imc()
-
-def fatorial(num):
-    i = num
-    while i > 1:
-
-        i -= 1
-        num *= i
-
-    return num
-
-x = input()
-
-print 'fatorial: ', fatorial(x)
-
+print("Media Final: ", media)
